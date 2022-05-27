07481ffad2ca2fe5abba495d788cd993575206ed
Marcelo Colla <marcelo.colla@contasimples.com>
2022-05-27 19:59:03 -0300

    adicionando estrutura inicial

---
+++ src/bootstrap.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+function MainApp() {
+  return <h1>Micro FrontEnd: App</h1>
+}
+
+render(<MainApp />, document.getElementById('app'))
