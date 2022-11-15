6494ae39d7623917db31659604a61279b785eb93
Marcelo Colla <marcelo.colla@contasimples.com>
2022-11-15 11:56:16 -0300

    adiciona script para analise de mantenabilidade

---
+++ metrics/plato-analyze.js
@@ -0,0 +1,40 @@
+const plato = require('plato-analyze')
+const pkg = require('../package.json')
+
+//be sure and set your src, output, and any options.
+const src = [
+  './dist/app/**/*.js',
+  './dist/components/**/*.js',
+  './dist/helpers/**/*.js',
+  './dist/pages/**/*.js',
+  './dist/services/**/*.js',
+]
+const outputDir = './metrics/artifacts'
+
+const platoArgs = {
+  title: pkg.name,
+  eslint: {},
+}
+
+//you can use the reports in the callback.
+function callback(reports) {
+  const overview = plato.getOverviewReport(reports)
+
+  const { total, average } = overview.summary
+
+  const output = `\n\nOUTPUT:
+    ----------------------
+    eslint: ${total.eslint}
+    sloc: ${total.sloc}
+    maintainability: ${total.maintainability}
+    average
+    ----------------------
+    eslint: ${average.eslint}
+    sloc: ${average.sloc}
+    maintainability: ${average.maintainability}`
+
+  console.log(output)
+}
+
+// usage is plato.inspect
+plato.inspect(src, outputDir, platoArgs, callback)
